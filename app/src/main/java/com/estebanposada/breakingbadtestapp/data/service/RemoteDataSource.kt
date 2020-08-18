b61916da3238026b8d975457a99c9b662f070260
Esteban posada <esteban.posada.dev@gmail.com>
2020-08-18 13:13:40 -0500

    Removing unnecessary code.

--- app/src/main/java/com/estebanposada/breakingbadtestapp/data/service/RemoteDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.estebanposada.breakingbadtestapp.data.service
-
-import com.estebanposada.breakingbadtestapp.data.service.model.Character
-
-interface RemoteDataSource {
-    suspend fun getCharacters():List<Character>
-}
